11d0e5dfe507fd001c9ac7883a14222524a14588
fyolawahyu <95853154+fyolawahyu@users.noreply.github.com>
2021-12-15 19:32:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ df2 = pd.DataFrame(df,columns= ['kode_negara','tahun','produksi'])
 df2=df2.loc[df2['kode_negara']=="AUS"]
 df2['produksi'] = pd.to_numeric(df2['produksi'], errors='coerce')
 
-left_col.df2(df.head(n_tampil))
+left_col.markdown(df2)
 
 fig, ax = plt.subplots()
 ax.plot(df2['tahun'], df2['produksi'], label = df2['tahun'])
@@ -47,5 +47,5 @@ ax.set_xlabel("Tahun", fontsize = 12)
 ax.set_ylabel("Jumlah Produksi", fontsize = 12)
 ax.legend(fontsize = 2)
 plt.show
-left_col.pyplot(fig)
+right_col.pyplot(fig)
 
