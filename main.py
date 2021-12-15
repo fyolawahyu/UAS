5f6d29ffc9169e15dcf159e586dc147a477413f3
fyolawahyu <95853154+fyolawahyu@users.noreply.github.com>
2021-12-15 22:29:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,20 +15,21 @@ with open("kode_negara_lengkap.json", "r") as read_file:
 print(data[0])
 dfJ = pd.DataFrame(data)
 
-
 #READ DATA CSV
 csv = pd.read_csv("produksi_minyak_mentah.csv")
 df = pd.DataFrame(csv)
 print(df)
 
+#MEMBUAT DATA FRAME TIAP FILE
 st.title('Data Produksi Minyak Mentah')
 st.header('UAS Pemrograman Komputer')
 ch_ = csv_('produksi_minyak_mentah.csv')
 jh_ = json_('kode_negara_lengkap.json')
-df_ = ch_.dataFrame
+csv_ = ch_.dataFrame
 df_info = jh_.dataFrame
 negara_li = df_info['name'].tolist()
 
+#MENGATUR LETAK OUTPUT
 st.sidebar.title("Pengaturan")
 left_col, mid_col, right_col = st.columns(3)
 negara = st.sidebar.selectbox('Pilih negara : ',negara_li) 
@@ -38,17 +39,19 @@ kode = df_info[df_info['name']==negara]['alpha-3'].tolist()[0]
 st.sidebar.write('Kode negara : ',kode)
 st.sidebar.write('Negara : ',negara)
 
-# Mengubah string menjadi float pada data produksi
+# MENGUBAH STRING MENJADI FLOAT
 df['produksi'] = df['produksi'].astype(str).str.replace(".", "", regex=True).astype(float)
 df['produksi'] = df['produksi'].astype(str).str.replace(",", "", regex=True).astype(float)
 df['produksi'] = pd.to_numeric(df['produksi'], errors='coerce')
 
+#OUTPUT TABEL A
 df2 = pd.DataFrame(df,columns= ['kode_negara','tahun','produksi'])
 df2=df2.loc[df2['kode_negara']==kode]
 df2['produksi'] = pd.to_numeric(df2['produksi'], errors='coerce')
 
 left_col.write(df2)
 
+#OUTPUT GRAFIK A
 fig, ax = plt.subplots()
 ax.plot(df2['tahun'], df2['produksi'], label = df2['tahun'])
 ax.set_title("Jumlah Produksi Per Tahun")
@@ -57,4 +60,16 @@ ax.set_ylabel("Jumlah Produksi", fontsize = 12)
 ax.legend(fontsize = 2)
 plt.show
 mid_col.pyplot(fig)
+
+#--b--
+tahun = int(input("Pilih Tahun : "))
+n = ("Pilih Banyak Negara : ")
+
+dfb = csv_.loc[csv_['tahun'] == tahun][:n]
+dfb = dfb.sort_values(by='produksi', ascending = False)
+dfB = dfB[:n]
+
+dfB.plot.bar('kode negara',produksi)
+plt.show
+left_col.pyplot(fig)
 
