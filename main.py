3a53c1a2e5acf50295898add77d745764e65fbac
fyolawahyu <95853154+fyolawahyu@users.noreply.github.com>
2021-12-15 22:51:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ dfb = csv_.loc[csv_['tahun'] == tahun][:n]
 dfb = dfb.sort_values(by='produksi', ascending = False)
 dfb = dfb[:n]
 
-dfb.plot.bar('kode_negara','produksi')
+dfb.plot.bar(x='kode_negara',y='produksi')
 plt.show
 left_col.pyplot(dfb)
 
