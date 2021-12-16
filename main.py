4917c08d79bf04c31289539fd5ad24004726c211
fyolawahyu <95853154+fyolawahyu@users.noreply.github.com>
2021-12-16 20:40:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -172,7 +172,7 @@ st.write(region_negara)
 st.write(subregion_negara)
                                     
 dfakumulatifmin=dk[dk.kumulatif !=0]
-dfakumulatifmin = dfakumulatifmin[:1].sort_values(by=['kumulatif'], descending = True)
+dfakumulatifmin = dfakumulatifmin[:1].sort_values(by=['kumulatif'], ascending = False)
 jumlah_produksi = dfakumulatifmin[:1].iloc[0]['kumulatif']
 kode_negara = dfakumulatifmin[:1].iloc[0]['kode_negara']
 nama_negara = ""
