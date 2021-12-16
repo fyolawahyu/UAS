7c6ba90d7d214058ddd4f5a47d238195242b0130
fyolawahyu <95853154+fyolawahyu@users.noreply.github.com>
2021-12-16 07:40:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -69,6 +69,6 @@ dfb = df.loc[df['tahun'] == tahun][:n]
 dfb = dfb.sort_values(by='produksi', ascending = False)
 dfb = dfb[:n]
 
-dfb.plot.bar(x='kode_negara', y='produksi')
+dfb.plot.bar(x='negara', y='produksi')
 plt.show()
 st.pyplot(plt)
