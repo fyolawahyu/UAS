75261b581d7f4312183c73e9c16304e1dd79064c
fyolawahyu <95853154+fyolawahyu@users.noreply.github.com>
2021-12-16 07:55:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ df2 = pd.DataFrame(df,columns= ['kode_negara','tahun','produksi'])
 df2=df2.loc[df2['kode_negara']==kode]
 df2['produksi'] = pd.to_numeric(df2['produksi'], errors='coerce')
 
-left_col.write(df2)
+st.write(df2)
 
 #OUTPUT GRAFIK A
 fig, ax = plt.subplots()
@@ -59,7 +59,7 @@ ax.set_xlabel("Tahun", fontsize = 12)
 ax.set_ylabel("Jumlah Produksi", fontsize = 12)
 ax.legend(fontsize = 2)
 plt.show
-mid_col.pyplot(fig)
+st.pyplot(fig)
 
 #--b--
 n = st.sidebar.number_input("Berapa besar negara?", min_value=1, max_value=None)
@@ -71,4 +71,4 @@ dfb = dfb[:n]
 
 dfb.plot.bar(x='kode_negara', y='produksi')
 plt.show()
-left_col.pyplot(plt)
+st.pyplot(plt)
