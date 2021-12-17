9737634575ba93dece2531096d46c3536379359f
fyolawahyu <95853154+fyolawahyu@users.noreply.github.com>
2021-12-17 08:04:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+[theme]
+primaryColor="#F63366"
+backgroundColor="#FFFFFF"
+secondaryBackgroundColor="#F0F2F6"
+textColor="#262730"
+font="sans serif"
+
 #IMPORT AWAL
 import json
 import pandas as pd
